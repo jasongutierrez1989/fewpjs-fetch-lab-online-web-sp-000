126638b80bcf1ee116f5721518b355894e5e32d9
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-21 03:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function fetchBooks('https://anapioficeandfire.com/api/books') {
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json));
 }
